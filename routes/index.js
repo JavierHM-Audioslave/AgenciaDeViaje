fd8e816aa20bc7a794a798095a50116a66f3d4df
JavierHM-Audioslave <javorengero2000@gmail.com>
2021-01-05 16:12:35 -0300

    Enviando un dato a la vista 'nosotros.pug', y creación de vista 'inicio.pug'

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ import express, { Router } from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.send("Inicio");
+    res.render("inicio");
 });
 
 router.get("/nosotros", (req, res) => {
     
-    res.render("nosotros");
+    const viajes = "Viaje a Argentina";
+    res.render("nosotros", {
+          textoViajes: viajes // Esta línea es un nombreDeVariableAEnviarALaVista: nombreDeLaVariableQueTieneElContenidoActualmente. Si el nombre de la clave debe ser igual al nombre de la variable con el valor, entonces en esta línea sólo pondriamos "viajes". //
+    });
 });
 
 export default router;
