c6cd6335ae59fadc5ba855a7ec21385b9b38ff5f
JavierHM-Audioslave <javorengero2000@gmail.com>
2021-01-05 15:30:01 -0300

    Refactoreo: routers enviados a index.js dentro de carpeta routes

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import router from "./routes/index.js"
 
 const app = express();
 
 // Definición de puerto //
 const port = process.env.PORT || 4000;
+
+// Agregar router //
+app.use("/", router);
 
 app.listen(port, () => console.log(`El servidor está funcionando en el puerto ${port}`) );
