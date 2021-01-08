cfc6f88c2439827a4fb7fa8a65609d27163a3d70
JavierHM-Audioslave <javorengero2000@gmail.com>
2021-01-08 16:49:48 -0300

    Agregado de controlador que actúa en URL 'testimoniales' con método POST. Este controlador verifica que lo enviado sea correcto para luego guardar el testimonio del usuario en la BD (no está implementado ésto todavía), aunque si no es correcto, se carga la misma vista 'testimoniales.pug' la cual mostrará los errores y cargará, dónde corresponda, los datos bien ingresados

---
+++ controllers/testimonialController.js
@@ -0,0 +1,31 @@
+export const guardarTestimonial = (req, res) => {
+
+    const {nombre, correo, mensaje} = req.body;
+    // console.log(nombre + "-" + "correo" + "-" + mensaje);
+    const errores = [];
+
+    if( nombre.trim() === "" ) {
+        errores.push({mensaje: "No se ha ingresado el nombre"});
+    }
+    if( correo.trim() === "" ) {
+        errores.push({mensaje: "No se ha ingresado el correo"})
+    }
+    if( mensaje.trim() === "" ) {
+        errores.push({mensaje: "No se ha ingresado el mensaje"})
+    }
+
+    if( errores.length > 0 ) {
+        res.render("testimoniales", {
+            pagina: "Testimoniales",
+            errores,
+            nombre,
+            correo,
+            mensaje
+        })
+    } else {
+        // HACER MAÑANA: el bloque 'else' debe guardar en la base de datos y ver qué vista mostrar porque la que muestra ahora la puse sólo para que devuelva algo //
+        res.render("testimoniales", {
+            pagina: "TESTIMONIO INGRESADO CORRECTAMENTE"
+        })
+    }
+}
